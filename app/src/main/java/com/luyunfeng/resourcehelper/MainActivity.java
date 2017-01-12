88ea79e8430936aa40ee5ef3d55e2dfdaf5b5c9c
卢运烽 <414675087@qq.com>
2017-01-12 22:33:28 +0800

    Simple usage

--- app/src/main/java/com/luyunfeng/resourcehelper/MainActivity.java
+++ app/src/main/java/com/luyunfeng/resourcehelper/MainActivity.java
@@ -1,7 +1,22 @@
 package com.luyunfeng.resourcehelper;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+/**
+ * Created by luyunfeng on 2017/1/10.
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv_1 = (TextView) findViewById(R.id.tv_1);
+        TextView tv_2 = (TextView) findViewById(R.id.tv_2);
+        TextView tv_3 = (TextView) findViewById(R.id.tv_3);
+        TextView tv_4 = (TextView) findViewById(R.id.tv_4);
+        final TextView tv_5 = (TextView) findViewById(R.id.tv_5);
+        TextView tv_6 = (TextView) findViewById(R.id.tv_6);
+        TextView tv_7 = (TextView) findViewById(R.id.tv_7);
+
+
+        tv_1.setTextSize(ResourceHelper.getSpPixelSize(R.dimen.large_text_size));
+
+
+        // 当然可以直接用StringRes设置，tv_2.setText(R.string.app_name);
+        tv_2.setText(ResourceHelper.getString(R.string.app_name));
+
+
+        String[] demo = ResourceHelper.getStringArray(R.array.demo);
+        tv_3.setText(TextUtils.join(" ", demo));
+
+
+        tv_4.setTextColor(ResourceHelper.getColor(R.color.colorAccent));
+
+
+        tv_5.setTextColor(ResourceHelper.getColorStateList(R.color.normal_color_selector));
+        tv_5.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                tv_5.setEnabled(false);
+            }
+        }, 2000);
+
+
+        StringBuilder builder = null;
+        try {
+            InputStream inputStream = ResourceHelper.getAssets().open("test.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+            builder = new StringBuilder();
+            String str;
+            while ((str = br.readLine()) != null) {
+                builder.append(str);
+            }
+            br.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (builder == null) builder = new StringBuilder("ic_launcher");
+            tv_6.setText(builder.toString());
+
+
+        int drawableId = ResourceHelper.getIdentifier(builder.toString(), "drawable");
+        // int drawableId = R.drawable.ic_launcher;
+        Drawable drawable = ResourceHelper.getDrawable(drawableId);
+        int size = (int) ResourceHelper.getDimension(R.dimen.activity_vertical_margin);
+        drawable.setBounds(0, 0, size, size);
+
+        tv_7.setCompoundDrawablePadding(ResourceHelper.getDimensionPixelSize(R.dimen.activity_vertical_margin));
+        tv_7.setCompoundDrawables(drawable, null, null, null);
     }
 }
--- app/src/main/java/com/luyunfeng/resourcehelper/ResourceHelper.java
+++ app/src/main/java/com/luyunfeng/resourcehelper/ResourceHelper.java
@@ -1,9 +1,18 @@
 package com.luyunfeng.resourcehelper;
 
+import android.annotation.SuppressLint;
+import android.app.Application;
 import android.content.Context;
+import android.content.res.AssetManager;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.support.annotation.ArrayRes;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringDef;
 import android.support.annotation.StringRes;
 
 /**
@@ -12,9 +21,12 @@ import android.support.annotation.StringRes;
 
 public class ResourceHelper {
 
+    private static Context context;
+
     private static Resources resources;
 
     public static void init(Context context) {
+        ResourceHelper.context = context;
         resources = context.getResources();
     }
 
@@ -22,12 +34,22 @@ public class ResourceHelper {
         return resources;
     }
 
-    public static float getDimension(int resId) {
+    public static float getDimension(@DimenRes int resId) {
         return resources.getDimension(resId);
     }
 
-    public static int getDimensionPixelSize(int resId) {
+    /**
+     * 获取dp对应的像素值
+     */
+    public static int getDimensionPixelSize(@DimenRes int resId) {
         return resources.getDimensionPixelSize(resId);
+    }
+
+    /**
+     * 获取sp对应的像素值
+     */
+    public static float getSpPixelSize(@DimenRes int resId) {
+        return resources.getDimension(resId) / resources.getDisplayMetrics().scaledDensity;
     }
 
     public static String getString(@StringRes int resId) {
@@ -38,15 +60,38 @@ public class ResourceHelper {
         return resources.getStringArray(resId);
     }
 
-    public static int getColor(int resId) {
-        return resources.getColor(resId);
+    public static int getColor(@ColorRes int resId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return resources.getColor(resId, context.getTheme());
+        } else {
+            return resources.getColor(resId);
+        }
     }
 
-    public static Drawable getDrawable(int resId) {
-        return resources.getDrawable(resId);
+    public static ColorStateList getColorStateList(@ColorRes int resId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return resources.getColorStateList(resId, context.getTheme());
+        } else {
+            return resources.getColorStateList(resId);
+        }
+    }
+
+    public static Drawable getDrawable(@DrawableRes int resId) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return resources.getDrawable(resId, context.getTheme());
+        } else {
+            return resources.getDrawable(resId);
+        }
+    }
+
+    public static AssetManager getAssets() {
+        return resources.getAssets();
     }
 
     public static int getIdentifier(String name, @IdentifierType String type) {
         return resources.getIdentifier(name, type, BuildConfig.APPLICATION_ID);
     }
+
+    @StringDef({"id", "drawable", "string"})
+    public @interface IdentifierType {}
 }
